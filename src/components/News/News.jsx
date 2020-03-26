c0d9a0e912d8b6204a135b128a49f672cca6e9e6
alexscub <alexscub1@gmail.com>
2020-03-26 22:43:05 +0200

    rev2_usingHooks

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import getNews from '../../services/api';
 import NewsCard from './NewsCard/NewsCard';
 
-export default class News extends Component {
-  state = { news: [] };
-
-  componentDidMount() {
-    getNews().then(data => this.setState({ news: data }));
-  }
-
-  render() {
-    const { news } = this.state;
-    return (
-      <div className="container">
-        <h2 className="title is-2 has-text-centered">Breaking News!</h2>
-        <div className="columns is-multiline">
-          {news.map(item => (
-            <NewsCard key={item.publishedAt} {...item} />
-          ))}
-        </div>
+const News = () => {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    getNews().then(data => setNews([...data]));
+  }, []);
+  return (
+    <div className="container">
+      <h2 className="title is-2 has-text-centered">Breaking News!</h2>
+      <div className="columns is-multiline">
+        {news.map(item => (
+          <NewsCard key={item.publishedAt} {...item} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default News;
